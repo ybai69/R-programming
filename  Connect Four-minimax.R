dc52c9cc85abcdeacfb7ca9f24e0f2c6e57d5a98
ybai69 <bai42@wisc.edu>
2016-04-18 14:53:49 -0500

    change some comment lines

---  Connect Four-minimax.R
+++  Connect Four-minimax.R
@@ -3,22 +3,10 @@
 
 rm(list = ls())
 
-# Implement Connect Four in the same manner that we
-# implemented Tic-tac-toe in class. Start by implementing
-# the helper functions, below, and testing them by running
-#   source("hw3test.R")
-# Then write code for the game itself.
-#
-# We'll test your code by running
-#   source("hw3.R")
-# We might also play Connect Four, read your code, and do other tests.
-
-# Returns TRUE if vector v (of character strings) contains
-# (at least) four in a row of player (character string). e.g.
-#   four.in.a.row("X", c("O","X","X","X","X","O"))
-# is TRUE, while
-#   four.in.a.row("O", c("O","X","X","X","X","O"))
-# is FALSE.
+# connect four
+# Note that a user click in a column indicates that a checker should
+# go to that column's lowest empty row (unless the column is full).
+# using minimax algorithm, so the computer player can make a choice, not choosing the place randomly.
 four.in.a.row = function(player, v, debug=FALSE) {
   if (debug) {
     cat(sep="", "four.in.a.row(player=", player, ", v=", v, ")\n")
@@ -211,14 +199,6 @@ for (i in 1:(r*c)) {
   player = ifelse(test=(player == "X"), yes="O", no="X")
 }
 
-# Hint: this program is modeled on the tic-tac-toe program we did in
-# class, so studying the latter program is worthwhile.
 
-# Note that a user click in a column indicates that a checker should
-# go to that column's lowest empty row (unless the column is full).
 
-# Note that you should implement a computer player. At the least, it
-# should choose randomly from among the non-full columns. (Feel free
-# to do much more! If your computer player beats me on its first try,
-# you will earn a package of M&Ms. This is a hard task. Feel free to
-# ask for tips.)
+
